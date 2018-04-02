670c0ea44594b0a3ba9bdcd1ebc2ea600283b248
aleksandr <aleksandr.trokhimenko@devellar.com>
2018-04-02 10:59:17 +0300

    edit timer component

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,78 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TimerService} from "./timer.service";
 
 @Component({
-  selector: 'app-timer',
-  templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.css']
+    selector: 'app-timer',
+    templateUrl: './timer.component.html',
+    styleUrls: ['./timer.component.css']
 })
 export class TimerComponent implements OnInit {
 
-  constructor(private timerService: TimerService) { }
+    constructor(private timerService: TimerService) {
+    }
+
     timerSub;
     hour: number = 11;
     minute: number = 45;
     second: number = 58;
 
-    setHour:number;
+    setHour: number;
     setMinute: number;
     setSeconds: number;
 
-  ngOnInit() {
-      this.setHour = this.hour;
-      this.setMinute = this.minute;
-      this.setSeconds = this.second;
-  }
+    private initTimer() {
+        if (this.second < 59) {
+            this.second++;
+        } else {
+            this.second = 0;
+            if (this.minute < 59) {
+                this.minute++;
+            } else {
+                this.minute = 0
+                if (this.hour < 24) {
+                    this.hour++;
+                } else {
+                    this.hour = 0;
+                }
+            }
+        }
+    }
 
-
-
-  startTimer(){
-    this.timerSub = this.timerService.getTimer()
-        .subscribe((val) => {
-            this.initTimer();
-        })
-  }
-
-    stopTimer(){
-
+    private unsubscibeTimer() {
         if (this.timerSub) {
             this.timerSub.unsubscribe();
         }
+    }
 
+    ngOnInit() {
+        this.setHour = this.hour;
+        this.setMinute = this.minute;
+        this.setSeconds = this.second;
+    }
+
+
+    startTimer() {
+        this.timerSub = this.timerService.getTimer()
+            .subscribe((val) => {
+                this.initTimer();
+            })
+    }
+
+    stopTimer() {
+        this.unsubscibeTimer();
         this.hour = this.setHour;
         this.minute = this.setMinute;
         this.second = this.setSeconds;
     }
 
-    waitTimer(){
-        if (this.timerSub) {
-            this.timerSub.unsubscribe();
-        }
+    waitTimer() {
+        this.unsubscibeTimer();
     }
 
-    private initTimer(){
-            if( this.second < 59 ){
-                this.second++;
-            }else{
-                this.second = 0;
-                if( this.minute < 59 ){
-                    this.minute++;
-                } else{
-                    this.minute = '0' + 0
-                    if( this.hour < 24 ){
-                        this.hour++;
-                    }else{
-                        this.hour = 0;
-                    }
-                }
-            }
-    }
-
-    resetTimer(){
+    resetTimer() {
+        this.unsubscibeTimer();
         this.hour = 0;
         this.minute = 0;
         this.second = 0;
     }
-
-
 }
